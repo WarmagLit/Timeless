f4d87fa23b6bef135948e18a46450a643a41bd38
WarmagLit <genzeartem0@gmail.com>
2021-06-06 00:35:31 +0700

    First room version

---
+++ Assets/Scripts/TimeScriptOne.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Chronos;
+using UnityEngine;
+
+public class TimeScriptOne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the Enemies global clock
+        Clock clock = Timekeeper.instance.Clock("Character");
+
+        // Change its time scale on key press
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            clock.localTimeScale = -1; // Rewind
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            clock.localTimeScale = 0; // Pause
+            //clock.LerpTimeScale(0, 2, true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            clock.localTimeScale = 0.5f; // Slow
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            clock.localTimeScale = 1; // Normal
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            clock.localTimeScale = 2; // Accelerate
+        }
+    }
+}
